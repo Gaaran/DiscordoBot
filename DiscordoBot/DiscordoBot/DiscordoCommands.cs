d02d06517cfce84faf93500433cc2e98af4afa3a
Gaaran <44610788+Gaaran@users.noreply.github.com>
2018-11-17 16:24:21 +0100

    Add files via upload

---
+++ DiscordoBot/DiscordoBot/DiscordoCommands.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+
+namespace DiscordoBot
+{
+    class DiscordoCommands
+    {
+        
+        IReadOnlyList<DiscordMessage> readMessages = new List<DiscordMessage>();
+
+
+        [Command("purge")]
+        public async Task Purge(CommandContext commandContext, int getPurgeNumber = 0)
+        {
+
+            //await commandContext.RespondAsync($"purge last : {getPurgeNumber} messages");
+
+            readMessages = await commandContext.Channel.GetMessagesAsync(getPurgeNumber);
+
+            await commandContext.Channel.DeleteMessagesAsync(readMessages);
+            await commandContext.RespondAsync($"purge last : {getPurgeNumber} messages, nikez vous");
+        }
+
+        [Command("master")]
+        public async Task Master(CommandContext commandContext)
+        {
+            if (commandContext.User.IsCurrent)
+            {
+                await commandContext.RespondAsync($"Yes");
+            }
+            else
+                await commandContext.RespondAsync($"No");
+        }
+    }
+}
---
+++ DiscordoBot/DiscordoBot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+
+namespace DiscordoBot
+{
+    class Program
+    {
+
+        public static DiscordClient discord;
+        static CommandsNextModule commands;
+        //static DiscordMessage message;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+
+        static async Task MainAsync(string[] args)
+        {
+
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug,
+                Token = DiscordoBotToken.myBotToken,
+                TokenType = TokenType.Bot
+            });
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "!"
+            });
+            
+            commands.RegisterCommands<DiscordoCommands>();
+            
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}

dcf24cb274e5d4cc3124aa0e7f79807a8348e2bd
Yann <yan.sergent@gmail.com>
2018-12-13 23:35:56 +0100

    new clean commit

--- DiscordoBot/DiscordoBot/DiscordoCommands.cs
+++ DiscordoBot/DiscordoBot/DiscordoCommands.cs
@@ -14,18 +14,36 @@ namespace DiscordoBot
     {
         
         IReadOnlyList<DiscordMessage> readMessages = new List<DiscordMessage>();
-
+        IEnumerable<DiscordRole> discordRoles;
 
         [Command("purge")]
         public async Task Purge(CommandContext commandContext, int getPurgeNumber = 0)
         {
+            discordRoles = commandContext.Member.Roles;
+            
+            if (commandContext.User.Discriminator == "9008")
+            {
+                //await commandContext.RespondAsync($"purge last : {getPurgeNumber} messages");
 
-            //await commandContext.RespondAsync($"purge last : {getPurgeNumber} messages");
+                readMessages = await commandContext.Channel.GetMessagesAsync(getPurgeNumber +1 );
 
-            readMessages = await commandContext.Channel.GetMessagesAsync(getPurgeNumber);
+                await commandContext.Channel.DeleteMessagesAsync(readMessages);
+                await commandContext.RespondAsync($"purged last : {getPurgeNumber} messages, nikez vous");
+            }
+        }
 
-            await commandContext.Channel.DeleteMessagesAsync(readMessages);
-            await commandContext.RespondAsync($"purge last : {getPurgeNumber} messages, nikez vous");
+        [Command("test")]
+        public async Task Test(CommandContext commandContext)
+        {
+            
+            discordRoles = commandContext.Member.Roles;
+            
+            //discordRoles.GetEnumerator();
+            //if ( )
+            //{
+
+            //}
+            await commandContext.RespondAsync($"test : ");
         }
 
         [Command("master")]
